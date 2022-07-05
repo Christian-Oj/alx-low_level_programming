87c09d83aa9349d4418d040173169f72ac3c41e2
christian-Oj <christianojiezele@gmail.com>
2022-07-05 07:01:48 -0700

    Prints the positive number of any integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - returns only possitive values
+ * @c: is an integer value
+ * Return: 0 always
+ */
+int _abs(int c)
+{
+	int i;
+
+	i = abs(c);
+	return (i);
+}
