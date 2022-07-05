ef2fa29403df056247bfd4ba59742fffdcc1ef13
christian-Oj <christianojiezele@gmail.com>
2022-07-05 06:52:12 -0700

    Print Signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign - check if character is lowercase
+ * @n: is an integer value
+ * Return: 0 always
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
