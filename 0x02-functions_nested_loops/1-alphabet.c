4e0bef12310156fbfaa7b045923acce39d80698d
christian-Oj <christianojiezele@gmail.com>
2022-07-05 04:15:59 -0700

    Produces a function that converts uppercase leters to lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,23 +2,15 @@
 #include "main.h"
 
 /**
- * main -> calls a function print_alphabet
- * print_alpabet:does not return any value but prints localcase of the alphabets
+ * print_alpabet -> does not return any value but prints localcase of the alphabets
  * Return: 0 always
  */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
-	char result;
-	for (char c = 'A'; c <= 'Z' ; ++c)
+	char c;
+	for (c = 'a'; c <= 'z' ; ++c)
 	{
-		result = tolower(c);
-		_putchar(result);	
-	}
-	_putchar("\n");
+		_putchar(c);	
+	};
+	_putchar('\n');
 }
