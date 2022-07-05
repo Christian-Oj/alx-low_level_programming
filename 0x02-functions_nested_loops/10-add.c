25ef455d9c700dd261a7533d721c182be15a9278
christian-Oj <christianojiezele@gmail.com>
2022-07-05 11:48:50 -0700

    Add two intergers together and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * add - Add two integers and returns the result
+ * @i: integer value
+ * @j: integer value
+ * Return: 0 always
+ */
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+
+	return (result);
+}
