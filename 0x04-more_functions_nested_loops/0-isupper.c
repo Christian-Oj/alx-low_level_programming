442d95c9d94e1e9fbf6c641814494921a51d3def
christian-Oj <christianojiezele@gmail.com>
2022-07-08 08:05:21 -0700

    Print 1 if char is uppercase and 0 otherwise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,6 +6,8 @@
  * _isupper - checks if argument is uppercase
  * @c: integer parameter
  *
+ * Return: 1 if char is uppercase, 0 otherwise
+ *
  * Created by: Cristian-Oj
  * Creation Date: 08 July 2022
  */
