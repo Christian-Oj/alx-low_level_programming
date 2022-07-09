64110fa3abf1f9cbe1c9e68d5018320c826772d1
christian-Oj <christianojiezele@gmail.com>
2022-07-09 15:05:31 -0700

    All exercises done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-#include <ctype.h>
 
 /**
- * _isdigit - checks if argument is uppercase
- * @c: integer parameter
- *
- * Return: 1 if char is uppercase, 0 otherwise
- *
- * Created by: Cristian-Oj
- * Creation Date: 08 July 2022
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	int res;
-
-	res = isdigit(c) == 0? 0 : 1;
-	return (res);
+	return (c >= '0' && c <= '9');
 }
-
