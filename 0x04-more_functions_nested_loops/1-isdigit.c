3e9bf7f8e21e2721ce649abad15352900afdfb4a
christian-Oj <christianojiezele@gmail.com>
2022-07-09 13:46:24 -0700

    Is upper function

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,17 +3,19 @@
 #include <ctype.h>
 
 /**
- * _isupper - checks if argument is uppercase
+ * _isdigit - checks if argument is uppercase
  * @c: integer parameter
+ *
  * Return: 1 if char is uppercase, 0 otherwise
  *
  * Created by: Cristian-Oj
  * Creation Date: 08 July 2022
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	int res;
 
-	res = isupper(c) == 0? 0 : 1;
+	res = isdigit(c) == 0? 0 : 1;
 	return (res);
 }
+
